08fa976181ca0d3767f2cedb35921dbe79e98147
khushig111 <75142569+khushig111@users.noreply.github.com>
2022-02-02 14:28:24 +0530

    added day1 assignment

---
+++ D1-day1-assignment.py
@@ -0,0 +1,7 @@
+# KHUSHI GUPTA
+# Assignment Ques: Take multiple numbers through input and print the sum of the numbers.
+
+a,b,c = [int (i) for i in input("Enter three values to get sum:- ").split()]
+print("The numbers you gave are:- ", a,b,c)
+print("So the sum is:- ",a+b+c)
+# YOU MAY ENTER -VE VALUES AS WELL
